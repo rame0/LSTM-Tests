515308c96016f52d53c6958d994b1daef6e82925
rame0 <r@me0.biz>
2023-02-09 06:42:34 +0600

    Added shell script to startup on runpod.io

--- prepare_runpod.sh
+++ prepare_runpod.sh
@@ -2,8 +2,9 @@
 
 apt udpate
 apt upgrade -y
+apt install -y vim tmux htop
 
 apt install --reinstall python3-pip -y
 pip3 install --upgrade pip
 
-pip3 install numpy, scikit-learn, pandas
+pip3 install numpy scikit-learn pandas
